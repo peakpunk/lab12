c124fa4512aff2ef46b111c0a60e9bbf718f1b2d
peakpunk <alk357951@gmail.com>
2021-12-21 17:47:51 +0700

    complete lab 12

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,25 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<string>
+#include<ctime>
+using namespace std;
+
+int main(){
+    string grade;
+    int ra;
+    cout << "Press Enter 3 times to reveal your future.";
+    for (int i = 1; i <= 3; i++) cin.get();
+    srand(time(0));
+    ra = rand()%9;
+    if (ra == 0) grade =  "A";
+    else if (ra == 1) grade =  "B";
+    else if (ra == 2) grade =  "B+";
+    else if (ra == 3) grade =  "C";
+    else if (ra == 4) grade =  "C+";
+    else if (ra == 5) grade =  "D";
+    else if (ra == 6) grade =  "D+";
+    else if (ra == 7) grade =  "F";
+    else if (ra == 8) grade =  "W";
+    cout << "You will get " << grade << " in this 261102.";
+    return 0;
+}
